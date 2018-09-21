9becf746edc6c47c039dfeae3c099a293c391fb3
stygiansabyss <stygian.warlock.v2@gmail.com>
2018-09-21 00:32:21 -0500

    Updating navbar

--- app/Http/Composers/Menu.php
+++ app/Http/Composers/Menu.php
@@ -27,10 +27,10 @@ class Menu
     private function generateLeftMenu()
     {
         $leftMenu = \Menu::getMenu('leftMenu');
-
-        $leftMenu->link('home', function (Link $link) {
-            $link->name = 'Home';
-            $link->url  = route('home');
+        
+        $leftMenu->link('docs', function (Link $link) {
+            $link->name = 'Documentation';
+            $link->url  = route('larecipe.index');
         });
     }
 
